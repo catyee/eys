e204fc00628b587e171d03542516704e6441c669
euan <526379113@qq.com>
2023-04-13 17:38:43 +0800

    update

--- src/views/policy/text-contrast/index.vue
+++ src/views/policy/text-contrast/index.vue
@@ -28,26 +28,30 @@
       <div class="contrast-content">
         <div class="contrast-l ">
             <div class="item flex">
-              <span>文本名称：</span>
-              <span></span>
+              <span class="item-title">文本名称：</span>
+              <span>{{title1}}</span>
+            </div>
+            <div class="item flex">
+              <span class="item-title">高频词：</span>
+              <span>{{frequentWord1}}</span>
             </div>
         </div>
         <div class="contrast-r">
           <div class="item flex">
-            <span>文本名称：</span>
-            <span></span>
+            <span class="item-title">文本名称：</span>
+            <span>{{title2}}</span>
           </div>
           <div class="item flex">
-            <span>高频词：</span>
-            <span></span>
+            <span class="item-title">高频词：</span>
+            <span>{{frequentWord2}}</span>
           </div>
           <div class="item flex">
-            <span>增加的突发词：</span>
-            <span></span>
+            <span class="item-title">增加的突发词：</span>
+            <span>{{add?add: '无'}}</span>
           </div>
           <div class="item flex">
-            <span>减少的高频词：</span>
-            <span></span>
+            <span class="item-title">减少的高频词：</span>
+            <span>{{reduce?reduce: '无'}}</span>
           </div>
         </div>
       </div>
@@ -67,7 +71,13 @@ export default {
     return {
       // 相似度
       similar: 0,
-      ids: null
+      ids: null,
+      title1: null,
+      title2: null,
+      frequentWord1: null,
+      frequentWord2: null,
+      add: null,
+      reduce: null
     }
   },
   created () {
@@ -86,6 +96,32 @@ export default {
       compareText(this.ids).then(res => {
         if (res.code === 200) {
           this.similar = res.similar ? Number((parseInt(res.similar * 100 * 100) / 100).toFixed(2)) : 0
+          this.title1 = res.title1
+          this.title2 = res.title2
+          try {
+            this.frequentWord1 = JSON.parse(res.frequentWord1)
+            this.frequentWord1 = Object.keys(this.frequentWord1).join(',')
+          } catch (e) {
+
+          }
+          try {
+            this.frequentWord2 = JSON.parse(res.frequentWord2)
+            this.frequentWord2 = Object.keys(this.frequentWord2).join(',')
+          } catch (e) {
+
+          }
+          try {
+            this.add = JSON.parse(res.add)
+            this.add = Object.keys(this.add).join(',')
+          } catch (e) {
+
+          }
+          try {
+            this.reduce = JSON.parse(res.reduce)
+            this.reduce = Object.keys(this.reduce).join(',')
+          } catch (e) {
+
+          }
         } else {
 
         }
